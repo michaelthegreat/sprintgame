c9e5d46e86b58f4112436b2914e3bf64b8006dd2
Michael Quintero <michaelq.quintero@gmail.com>
2018-03-30 23:05:13 -0400

    multiple enemies generated
    multiple enemies generated lol

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour {
+
+    public float scrollSpeed = 0.02f;
+    private float TIMER1_CONST = 4f;
+    float timer1;
+    private Animator animator;
+
+
+
+    //how many seconds  until you destroy this object
+    private float timeUntilDestroy = 20f;
+
+    // Use this for initialization
+    void Start () {
+        //Random rnd = new Random();
+        
+        //string resourcePath = "Assets/animations/Enemy " + randomEnemyNumber.ToString();
+        //Debug.Log(resourcePath);
+
+        //this.animator = this.GetComponent<Animator>();
+        //animator = Resources.Load("animations/Enemy " + randomEnemyNumber.ToString()) as Animator;*/
+        //this.GetComponent<Animator>().runtimeAnimatorController = Resources.Load(resourcePath) as RuntimeAnimatorController;
+        animator = this.GetComponent<Animator>();
+        //Debug.Log(this.animator);
+        //
+        timer1 = TIMER1_CONST;
+    }
+
+ 
+    // Update is called once per frame
+    void Update () {
+        Vector3 position = this.transform.position;
+        position.x -= this.scrollSpeed;
+        this.transform.position = position;
+        timeUntilDestroy -= Time.deltaTime;
+        timer1 -= Time.deltaTime;
+        if (GameObject.FindGameObjectWithTag("score").GetComponent<Points>().point > 2)
+        {
+//            timer1 -= Time.deltaTime;
+
+            if (timer1 <= 0)
+            {
+                timer1 = TIMER1_CONST;
+                this.animator.SetTrigger("shooting");
+
+            }
+        }
+        if(timeUntilDestroy <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MasterScript.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterScript : MonoBehaviour {
+
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public bool pause;
+    public bool lose;
+    public float bottomYPosition;
+    public float topYPosition;
+    //Four rockets go out asynchriously
+    
+    public float TIMER1_CONST = 8f;
+    private float timer1;
+    float timer2 = .95F;
+    float timer3 = 1.25F;
+    float timer4 = 1.45F;
+    float current_timescale;
+    // Use this for initialization
+    void Start () {
+        timer1 = TIMER1_CONST;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!lose)
+        {
+
+            if (!pause)
+            {
+                //Accelerate(0.001f);
+                //insantiate rockets according to this script
+                //makeItHard();
+                this.generate();
+            }
+            else
+            {
+								//recalibrate input*/
+                //start_x = Input.acceleration.x;
+                //start_y = Input.acceleration.y;
+
+            }
+        }
+    }
+
+    bool pass1 = false;
+    bool pass2 = false;
+    void generate()
+    {
+        if (GameObject.FindGameObjectWithTag("score").GetComponent<Points>().point > 2)
+        {
+            timer1 -= Time.deltaTime;
+            //timer2 -= Time.deltaTime;
+            //timer3 -= Time.deltaTime;
+            //timer4 -= Time.deltaTime;
+            if (timer1 <= 0)
+            {
+                timer1 = TIMER1_CONST;
+                generateEnemy();
+
+            }/*
+            if (timer2 <= 0)
+            {
+                timer2 = 95f;
+                generateEnemy();
+            }
+
+            if (timer3 <= 0)
+            {
+                timer3 = 1.2f;
+                generateEnemy();
+
+            }
+            if (timer4 <= 0)
+            {
+                timer4 = 1.4f;
+                generateEnemy();
+            }*/
+        }
+        /*
+        //Use these settings to adjust the difficulty
+        if (GameObject.FindGameObjectWithTag("score").GetComponent<Points>().point > 60 && !pass1)
+        {
+            current_timescale = 1.5f;
+            Time.timeScale = current_timescale;
+            pass1 = true;
+        }
+        else if (GameObject.FindGameObjectWithTag("score").GetComponent<Points>().point > 25 && !pass2)
+        {
+            current_timescale = 1.2f;
+            Time.timeScale = current_timescale;
+            pass2 = true;
+        }*/
+    }
+
+    void generateEnemy()
+    {
+        if (!lose)
+        {
+            Random.seed = (int)System.DateTime.Now.Ticks;
+            int randomEnemyNumber = Random.Range(1, 4);
+            Vector3 instants = new Vector3(11.0f, Random.Range(bottomYPosition, topYPosition), 0.0f);
+            //Debug.Log(randomEnemyNumber);
+            switch (randomEnemyNumber)
+            {
+                case 1: Instantiate(enemy1, instants, Quaternion.identity); break;
+                case 2: Instantiate(enemy2, instants, Quaternion.identity); break;
+                case 3: Instantiate(enemy3, instants, Quaternion.identity); break;
+                default: Instantiate(enemy1, instants, Quaternion.identity); break;
+
+            }
+            
+            //Randomly give double enemies because why not
+            //Adjust the range to set the difficulty
+            /*if (Random.Range(0, 5) == 4)
+            {
+                instants = new Vector3(11.0f, Random.Range(0f, 7.05f), 0.0f);
+                Instantiate(enemy, instants, Quaternion.identity);
+            }*/
+
+
+        }
+    }
+
+}
