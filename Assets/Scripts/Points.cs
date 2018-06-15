da0908af9441637c86a167ae07b3bf48099e5700
Michael Quintero <michaelq.quintero@gmail.com>
2018-06-14 20:54:07 -0400

    controls disabled and score stops increasing on loss.

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -5,19 +5,25 @@ public class Points : MonoBehaviour {
 
 	public int point = 0;
 	float timer = 0.0F;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private MasterScript masterScript;
+    // Use this for initialization
+    void Start () {
+        GameObject masterGameObject = GameObject.Find("MasterGameObject");
+        this.masterScript = masterGameObject.GetComponent<MasterScript>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		timer -= Time.deltaTime;
-		if( timer <= 0 )
-		{
-			point += 1;
-			timer = .25F;
-			GetComponent<GUIText>().text = "Score: " + point; //1/2 second
-		}
+
+        if (!this.masterScript.lose)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                point += 1;
+                timer = .25F;
+                GetComponent<GUIText>().text = "Score: " + point; //1/2 second
+            }
+        }
 	}
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,66 +7,80 @@ public class playerController : MonoBehaviour
 	public float speed = 10.0f;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-
+    public GameObject masterGameObject;
+    private MasterScript masterScript;
 
     // Use this for initialization
     void Start()
 	{
 		animator = this.GetComponent<Animator>();
+        GameObject masterGameObject = GameObject.Find("MasterGameObject");
+        this.masterScript = masterGameObject.GetComponent<MasterScript>();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-		//A NOTE ON ANIMATION TRANSITIONS.
-		//THE CHARACTER HAS THREE ANIMATION STATES: 
-		//Running, Shooting and Dead
-		//Make the character shoot by setting Shooting to 1
-		//Revert the character back to normal by setting Shooting to 0
-		//Kill the character by setting Dead to 1
-		//ex: animator.SetInteger("Shooting", 1);
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x--;
-			this.transform.position = position;
-            //this.bulletSpawn.position = position;
+
+        if (!this.masterScript.lose)
+        {
+            //Debug.Log("Lost");
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.x--;
+                this.transform.position = position;
+                //this.bulletSpawn.position = position;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.x++;
+                this.transform.position = position;
+                //this.bulletSpawn.position = position;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.y++;
+                this.transform.position = position;
+                //this.bulletSpawn.position = position;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.y--;
+                this.transform.position = position;
+                //this.bulletSpawn.position = position;
+            }
+            if (Input.GetKeyDown("space"))
+            {
+                animator.SetTrigger("Shooting");
+            }
         }
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.x++;
-			this.transform.position = position;
-            //this.bulletSpawn.position = position;
-		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y++;
-			this.transform.position = position;
-            //this.bulletSpawn.position = position;
+        else
+        {
+            
+
         }
-		if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y--;
-			this.transform.position = position;
-            //this.bulletSpawn.position = position;
-		}
-		if (Input.GetKeyDown ("space")) 
-		{
-            animator.SetTrigger("Shooting");
-        }
-		
-	}
+        //A NOTE ON ANIMATION TRANSITIONS.
+        //THE CHARACTER HAS THREE ANIMATION STATES: 
+        //Running, Shooting and Dead
+        //Make the character shoot by setting Shooting to 1
+        //Revert the character back to normal by setting Shooting to 0
+        //Kill the character by setting Dead to 1
+        //ex: animator.SetInteger("Shooting", 1);
+
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("player collision");
+        //Debug.Log("player collision");
         if (collision.gameObject.tag == "enemyBullet")
         {
             Destroy(collision.gameObject);
             animator.SetTrigger("dying");
             Destroy(GetComponent<PolygonCollider2D>());
+            this.masterScript.lose = true;
             //Destroy(gameObject,3.0f);
         }
     }
