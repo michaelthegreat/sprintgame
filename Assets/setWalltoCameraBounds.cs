c40b88f0187ca9cf97294385d4603612770e2f5d
Michael Quintero <michaelq.quintero@gmail.com>
2018-06-24 20:51:44 -0400

    boundaries added that are not set to camera bounds

---
+++ Assets/setWalltoCameraBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setWalltoCameraBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera mainCamera = Camera.main;
+        GameObject rightWall = GameObject.Find("rightWall");
+        GameObject leftWall = GameObject.Find("leftWall");
+        GameObject bottomWall = GameObject.Find("bottomWall");
+        GameObject topWall = GameObject.Find("topWall");
+        
+        float lowerBound= mainCamera.transform.position.y - mainCamera.orthographicSize;
+        Debug.Log(lowerBound);
+
+        //bottomWA.position = new Vector3(bottomWall.position.x, lowerBound, bottomWall.position.z);
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
